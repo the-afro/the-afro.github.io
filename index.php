9908d48e9c771d719a9d9fb42f1eaef03a886c14
the-afro <the-afro@users.noreply.github.com>
2018-05-30 23:38:30 +0300

    initial commit

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<title>TAK 123</title>
 </head>
 <body>
+	<?php $page = 'home';include 'include/header.php'; ?>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, laborum.</p>
 	<p>Ea suscipit quas repellendus consequatur beatae ipsa consequuntur pariatur, debitis.</p>
-	<p>Sed placeat consequuntur alias assumenda, eaque ex facere porro. Repudiandae.</p>
-	<p>Fugit fugiat doloribus maxime placeat expedita aliquid velit beatae fuga.</p>
-	<p>Assumenda aliquam dolor, cumque! Dolorem nostrum, aut asperiores. Asperiores, explicabo.</p>
 </body>
 </html>
