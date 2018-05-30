ba1578fbc19f9ecf21e96d9cb2dab03c4008b55e
the-afro <the-afro@users.noreply.github.com>
2018-05-30 23:39:42 +0300

    initial commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> 
 	<title>TAK 123</title>
 </head>
 <body>
